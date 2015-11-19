c9e4a338ccca50b6fc8cf591fc363ef3340fd4c4
Armin Ronacher <armin.ronacher@active-4.com>
2015-11-19 12:05:04 +0100

    Reformatted code and renamed package to avoid conflicts

--- test/test.js
+++ test/test.js
@@ -1,60 +1,60 @@
-"use strict";
+'use strict';
 
-var assert = require("assert");
-var babel = require("babel");
+var assert = require('assert');
+var babel = require('babel');
 
-var fs = require("fs");
+var fs = require('fs');
 
-describe("babel-gettext-plugin", function() {
+describe('babel-gettext-plugin', function() {
 
-    describe("#extract()", function() {
+  describe('#extract()', function() {
 
-        it("Should return a result", function() {
-            var result = babel.transform("let t = _t('code');_t('hello');", {
-                plugins: ["../index.js"],
-                extra: {
-                    gettext: {
-                        functionNames: {
-                           _t: ["msgid"]
-                        },
-                        fileName: "test.po"
-                    }
-                }
-            });
-            assert(!!result);
+    it('Should return a result', function() {
+      var result = babel.transform('let t = _t("code");_t("hello");', {
+        plugins: ['../index.js'],
+        extra: {
+          gettext: {
+            functionNames: {
+              _t: ['msgid']
+            },
+            fileName: 'test.po'
+          }
+        }
+      });
+      assert(!!result);
 
-            var content = fs.readFileSync("test.po");
-            assert(!!content);
-        });
-
-        it("No file", function() {
-            var result = babel.transform("let t = _t('code');_t('hello');", {
-                plugins: ["../index.js"],
-                extra: {
-                    gettext: {
-                        fileName: "test2.po"
-                    }
-                }
-            });
-
-            assert(!!result);
-            assert(!fs.existsSync("test2.po"));
-        });
-
-        it("Should return a result", function() {
-            var result = babel.transform("dnpgettext('mydomain', 'mycontext', 'msg', 'plurial', 10)", {
-                plugins: ["../index.js"],
-                extra: {
-                    gettext: {
-                        fileName: "test.po"
-                    }
-                }
-            });
-            assert(!!result);
-
-            var content = fs.readFileSync("test.po");
-            assert(!!content);
-        });
-
+      var content = fs.readFileSync('test.po');
+      assert(!!content);
     });
+
+    it('No file', function() {
+      var result = babel.transform('let t = _t("code");_t("hello");', {
+        plugins: ['../index.js'],
+        extra: {
+          gettext: {
+            fileName: 'test2.po'
+          }
+        }
+      });
+
+      assert(!!result);
+      assert(!fs.existsSync('test2.po'));
+    });
+
+    it('Should return a result', function() {
+      var result = babel.transform('dnpgettext("mydomain", "mycontext", "msg", "plurial", 10)', {
+        plugins: ['../index.js'],
+        extra: {
+          gettext: {
+            fileName: 'test.po'
+          }
+        }
+      });
+      assert(!!result);
+
+      var content = fs.readFileSync('test.po');
+      assert(!!content);
+    });
+
+  });
 });
